0e6fe5d984d78457c38c79f411e92cd747f969cf
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-02-28 13:27:02 +0200

    Task1 was completed

---
+++ tasks/task1.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let totalValue = 0;
+
+function calc(number, operation) {
+  if (operation === '+') {
+    totalValue += number;
+  } else if (operation === '-') {
+    totalValue -= number;
+  } else if (operation === '*') {
+    totalValue *= number;
+  } else if (operation === '/') {
+    totalValue /= number;
+  }
+
+  console.log(totalValue);
+}
+
+calc(5, "+"); // 5
+calc(2, "*"); // 10
+calc(3, "-"); // 7
+calc(2, "*"); // 14
+calc(7, "/"); // 2
