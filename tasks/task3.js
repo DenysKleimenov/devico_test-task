ffbc52e9c6dde0c0737a21e6ea094810a739ba2b
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-02-28 13:27:35 +0200

    Task3 was completed

---
+++ tasks/task3.js
@@ -0,0 +1,25 @@
+function bar(n) {
+  const matrix = [];
+
+  for (let row = 0; row < n; row++) {
+    matrix[row] = [];
+
+    for (let column = 0; column < n; column++) {
+      if (column === row || column + row === n - 1) {
+        matrix[row][column] = 2;
+      } else if (
+        (column > row && column + row < n - 1) ||
+        (row > column && row + column > n - 1)
+      ) {
+        matrix[row][column] = 1;
+      } else {
+        matrix[row][column] = 0;
+      }
+    }
+  }
+
+  return matrix;
+}
+
+console.log(bar(5));
+console.log(bar(4));
